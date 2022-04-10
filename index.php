44388a2ac5a11bdf783da152f8059b370f2e88af
Anurag sati <52669445+anuragsati@users.noreply.github.com>
2022-04-10 14:04:59 +0530

    added bootstrap

--- index.php
+++ index.php
@@ -33,39 +33,68 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Bootstrap JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
     <title>Todo List</title>
 </head>
 <body>
-    <div class="heading">Todo List</div>
-    <form action="index.php" method="POST">
-        <?php if(isset($errors)){ ?>
-            <p> <?php echo $errors; ?> </p>
-        <?php } ?>
+    <section class="min-vh-100" style="background-color: #eee;">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col col-lg-9 col-xl-7">
+                    <div class="card rounded-5">
+                        <div class="card-body p-4">
+                            <h4 class="text-center my-3 pb-3">To-Do List</h4>
+                            
+                            <?php if(isset($errors)){ ?>
+                                <div class="col-10 text-center text-danger"> <?php echo $errors; ?> </div>
+                            <?php } ?>
 
-        <input type="text" name="task" id="task-input">
-        <button type="submit" class="submit-btn" name="submit">Add Task</button>
-    </form>
+                            <form action="index.php" method="POST" class="row row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2">
+                                <div class="col-12">
+                                    <div class="form-outline">
+                                        <input type="text" name="task" class="form-control" placeholder="Enter a task here"/>
+                                    </div>
+                                </div>
 
-    <table>
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Tasks</th>
-                <th>Action</th>
-            </tr>
-        </thead>
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-primary" name="submit">Add</button>
+                                </div>
+                            </form>
 
-        <tbody>
-            <?php $i=1; while($row = mysqli_fetch_array($tasks)){ ?>
-                <tr>
-                    <td><?php echo $i; ?></td>
-                    <td><?php echo $row['task']; ?></td>
-                    <td>
-                        <a href="index.php?del-task=<?php echo $row['id']; ?>">X</a>
-                    </td>
-                </tr>
-            <?php $i++; } ?>
-        </tbody>
-    </table>
+                            <table class="text-center table mb-4">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Tasks</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <?php $i=1; while($row = mysqli_fetch_array($tasks)){ ?>
+                                        <tr>
+                                            <th scope="row"><?php echo $i; ?></th>
+                                            <td><?php echo $row['task']; ?></td>
+                                            <td>
+                                                <a class="btn btn-danger py-1 px-2" href="index.php?del-task=<?php echo $row['id']; ?>">
+                                                    Delete
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    <?php $i++; } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
